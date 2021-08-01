7d94a06909b7ea1263a2aed8bd66e59318e4402d
linuxonfire <jaimescampositzel@gmail.com>
2021-07-31 21:25:14 -0500

    First Commit

---
+++ getip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+# =^^=
+# Get eth0 ip
+# sh thisscript.sh eth0
+
+interface=$1
+echo "Interface: $interface"
+ip=$(ifconfig $interface | grep inet)
+# echo $ip | cut -d " " -f 2
+ip=$( echo $ip | cut -d " " -f 2)
+ip=$(echo $ip | cut -d ":" -f 2)
+echo "######getting ip address from eth0 interface#########"
+echo " ** Interface: $interface  ***"
+echo " ** IP Address: $ip ****"
+exit 0
